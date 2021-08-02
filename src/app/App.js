f55407c8851b82b3da4262967561e2fa97fd0d7d
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-02 20:38:48 -0300

    Pickers - configuration

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,16 @@
 import { ThemeProvider } from '@material-ui/core/styles'
+import { MuiPickersUtilsProvider } from '@material-ui/pickers'
+import MaterialUIPickers from './doc/MaterialUIPickers'
+import DateFnsUtils from '@date-io/date-fns'
 import theme from './config/theme'
-import TransferList from './doc/TransferList'
+import ptBR from 'date-fns/locale/pt-BR'
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <TransferList />
+      <MuiPickersUtilsProvider utils={DateFnsUtils} locale={ptBR}>
+        <MaterialUIPickers />
+      </MuiPickersUtilsProvider>
     </ThemeProvider>
   )
 }
