63caff0e48dcb0b2e2f50b15f96c7da9f97dedde
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-05 21:28:57 -0300

    Ajustes

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,15 @@
 import { ThemeProvider } from '@material-ui/core/styles'
 import { MuiPickersUtilsProvider } from '@material-ui/pickers'
-import MaterialUIPickers from './doc/MaterialUIPickers'
 import DateFnsUtils from '@date-io/date-fns'
 import theme from './config/theme'
 import ptBR from 'date-fns/locale/pt-BR'
+import DashboardPage from './demos/dashboard/DashboardPage'
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <MuiPickersUtilsProvider utils={DateFnsUtils} locale={ptBR}>
-        <MaterialUIPickers />
+        <DashboardPage />
       </MuiPickersUtilsProvider>
     </ThemeProvider>
   )
